8db82fff18b441da2123e8813226806a7cf59e83
Valentyn Kovalchuk <kovalhood@gmail.com>
2023-05-23 10:30:45 +0300

    Added lesson structure

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,29 @@ import { GlobalStyle } from './GlobalStyle';
 import { Layout } from './Layout/Layout';
 import { RecipeForm } from './RecipeForm/RecipeForm';
 
+// render > didMount > getItem > setState > update > render > didUpdate > setItem
+
 export class App extends Component {
   state = {
-    recipes: initialRecipes,
+    recipes: [],
   };
+
+  componentDidMount() {
+    const savedRecipes = localStorage.getItem('recipes');
+    if (savedRecipes !== null) {
+      // If something has already been saved in LS, we write THIS in the state
+      this.setState({ recipes: JSON.parse(savedRecipes) });
+    } else {
+      // If there is nothing in LS yet, we write in state initialRecipes
+      this.setState({ recipes: initialRecipes });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.recipes !== this.state.recipes) {
+      localStorage.setItem('recipes', JSON.stringify(this.state.recipes));
+    }
+  }
 
   addRecipe = newRecipe => {
     this.setState(prevState => ({
@@ -23,6 +42,7 @@ export class App extends Component {
   };
 
   render() {
+    console.log('render');
     return (
       <Layout>
         <RecipeForm onSave={this.addRecipe} />
